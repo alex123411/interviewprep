ca55918c2478fb29c7447a2cb21fc5d24db8a266
alex123411 <alextorgovskyh@gmail.com>
2024-01-30 21:05:03 -0600

    rebase

--- src/main/java/org/alex123411/interviewprep/Solution/DailyTask/Medium.java
+++ src/main/java/org/alex123411/interviewprep/Solution/DailyTask/Medium.java
@@ -181,46 +181,4 @@ public class Medium {
             maxLengthHelper(arr, index + 1, current, res);
         }
     }
-
-    public int maxWidthOfVerticalArea(int[][] points) {
-        int res = 0;
-
-        int[] axis = new int[points.length];
-        for (int i = 0; i < points.length; i++) axis[i] = points[i][0];
-
-        Arrays.sort(axis);
-
-        for (int i = 1; i < points.length; i++) {
-            res = Math.max(res, axis[i] - axis[i - 1]);
-        }
-        return res;
-    }
-
-    // 1239. Maximum Length of a Concatenated String with Unique Characters
-    // https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/
-    public int maxLength(List<String> arr) {
-        int[] res = new int[1];
-
-        maxLengthHelper(arr, 0, new StringBuilder(), res);
-        return res[0];
-    }
-
-    public void maxLengthHelper(List<String> arr, int index, StringBuilder current, int[] res) {
-        if (index >= arr.size()) return;
-        res[0] = Math.max(res[0], current.length());
-        int[] chars = new int[26];
-        for (char c : current.toString().toCharArray()) chars[c - 'a'] = 1;
-        boolean contains = false;
-        for (char c : arr.get(index).toCharArray()) {
-            contains = chars[c - 'a'] != 0;
-            if (contains) break;
-        }
-        if (contains) return;
-
-        for (int i = index; i < arr.size(); i++) {
-            current.append(arr.get(index));
-            maxLengthHelper(arr, index + 1, current, res);
-        }
-    }
-
 }
